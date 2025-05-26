d74177f1b97b26670160b6474b1004ff394a9dc6
ankitkumarsingh1702 <ankitbehappy@gmail.com>
2025-05-27 03:34:32 +0530

    chore(settings): update generated settings API data source

--- lib/app/platforms/mobile/settings/data/data_sources/settings_page_api_data_source.g.dart
+++ lib/app/platforms/mobile/settings/data/data_sources/settings_page_api_data_source.g.dart
@@ -6,44 +6,47 @@ part of 'settings_page_api_data_source.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _SettingsPageApiDataSource implements SettingsPageApiDataSource {
   _SettingsPageApiDataSource(
     this._dio, {
     this.baseUrl,
+    this.errorLogger,
   });
 
   final Dio _dio;
 
   String? baseUrl;
 
+  final ParseErrorLogger? errorLogger;
+
   @override
   Future<HttpResponse<dynamic>> afterUsageInserted(
       List<AppUsageData> appUsageRequests) async {
-    const _extra = <String, dynamic>{};
+    final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     final _data = appUsageRequests.map((e) => e.toJson()).toList();
-    final _result =
-        await _dio.fetch(_setStreamType<HttpResponse<dynamic>>(Options(
+    final _options = _setStreamType<HttpResponse<dynamic>>(Options(
       method: 'POST',
       headers: _headers,
       extra: _extra,
     )
-            .compose(
-              _dio.options,
-              '/app-usage/after-usage-inserted',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(
-                baseUrl: _combineBaseUrls(
-              _dio.options.baseUrl,
-              baseUrl,
-            ))));
-    final value = _result.data;
-    final httpResponse = HttpResponse(value, _result);
+        .compose(
+          _dio.options,
+          '/app-usage/after-usage-inserted',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch(_options);
+    final _value = _result.data;
+    final httpResponse = HttpResponse(_value, _result);
     return httpResponse;
   }
 
